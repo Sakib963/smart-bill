1ea18a9b120ae8b8532c14177899553f3a920fdd
Sakib963 <abdullah15-963@diu.edu.bd>
2025-02-10 21:06:29 +0600

    project creation

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}", // Include all Angular templates and components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
